51a0091fd2fccdc3f5bd52a8fa8bdf0c1da462af
Luka <luka.akhalkatsi.1@btu.edu.ge>
2025-07-25 12:50:02 -0700

    offers

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,8 +6,8 @@ function Item({ id, name, image, new_price, old_price }) {
       <img src={image} alt="" />
       <p>{name}</p>
       <div className="item-prices">
-        <div className="item-price-new">{new_price}</div>
-        <div className="item-price-old">{old_price}</div>
+        <div className="item-price-new">${new_price}</div>
+        <div className="item-price-old">${old_price}</div>
       </div>
     </div>
   );
