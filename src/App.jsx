60bb89e50a12ca05378f3c17c06301b9152172bd
Luka <luka.akhalkatsi.1@btu.edu.ge>
2025-07-30 12:57:39 -0700

    shopcategory done

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
 import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -16,9 +19,20 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/men" element={<ShopCategory category="men" />} />
-          <Route path="/women" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory catgeory="kids" />} />
+          <Route
+            path="/men"
+            element={<ShopCategory ItemCategory="men" banner={men_banner} />}
+          />
+          <Route
+            path="/women"
+            element={
+              <ShopCategory ItemCategory="women" banner={women_banner} />
+            }
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory ItemCategory="kid" banner={kid_banner} />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,5 +1,45 @@
-function ShopCategory() {
-  return <div>ShopCategory</div>;
+import { useContext } from "react";
+import "./CSS/ShopCategory.css";
+import { ShopContext } from "../Context/ShopContext";
+import dropdown_icon from "../Components/Assets/dropdown_icon.png";
+import Item from "../Components/Item/Item";
+
+function ShopCategory({ ItemCategory, banner }) {
+  const { all_product } = useContext(ShopContext);
+  return (
+    <div className="shop-category">
+      <img className="shopcategory-banner" src={banner} alt="" />
+      <div className="shopcategory-indexSort">
+        <p>
+          <span>Showing 1-12</span> out of 36 products
+        </p>
+        <div className="shopcategory-sort">
+          Sort by <img src={dropdown_icon} alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {all_product.map(
+          ({ id, name, image, new_price, old_price, category }, i) => {
+            if (ItemCategory === category) {
+              return (
+                <Item
+                  key={i}
+                  id={id}
+                  name={name}
+                  image={image}
+                  new_price={new_price}
+                  old_price={old_price}
+                />
+              );
+            } else {
+              return null;
+            }
+          }
+        )}
+      </div>
+      <div className="shopcategory-loadmore">Explore More</div>
+    </div>
+  );
 }
 
 export default ShopCategory;
