5519d318e94eb93e6ffaab733f63676b5ba0540e
Luka <luka.akhalkatsi.1@btu.edu.ge>
2025-08-04 13:03:22 -0700

    signup

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,5 +1,26 @@
+import "./CSS/LoginSignup.css";
+
 function LoginSignup() {
-  return <div>login</div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login Here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default LoginSignup;
