dc4fb9134201683cab2e93e6c84559c5b1fe98ac
Luka <luka.akhalkatsi.1@btu.edu.ge>
2025-09-04 10:45:19 -0700

    demo cart items display

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,5 +1,6 @@
 import "./CartItems.css";
 import remove_icon from "../Assets/cart_cross_icon.png";
+import all_product from "../Assets/all_product.js";
 
 function CartItems() {
   return (
@@ -13,17 +14,32 @@ function CartItems() {
         <p>Remove</p>
       </div>
       <hr />
-      <div>
-        <div className="cartitems-format">
-          <img src="" alt="" className="carticon-product-icon" />
-          {/* <p>product name</p> */}
-          {/* <p>product new price</p> */}
-          <button className="cartitems-quantity"></button>
-          {/* <p>total value of individual item</p> */}
-          <img src={remove_icon} alt="" />
-        </div>
-        <hr />
-      </div>
+      {all_product.map((item) => {
+        if (item.id === 1 || item.id === 2) {
+          return (
+            <div>
+              <div className="cartitems-format caritems-format-main">
+                <img
+                  src={item.image}
+                  alt=""
+                  className="carticon-product-icon"
+                />
+                <p>{item.name}</p>
+                <p>${item.old_price}</p>
+                <button className="cartitems-quantity">3</button>
+                <p>$255</p>
+                <img
+                  className="cartitems-remove-icon"
+                  src={remove_icon}
+                  alt=""
+                />
+              </div>
+              <hr />
+            </div>
+          );
+        }
+        return null;
+      })}
     </div>
   );
 }
